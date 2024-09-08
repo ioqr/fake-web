9a4b046847374e4fdf036612e2f0a110bc7cf84a
Evan Ram <evan@evanram.com>
2024-09-08 20:30:25 +0000

    perf: use np.random.Generator(np.random.PCG64()) instead of np.random.choice

--- fake-web.py
+++ fake-web.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import uuid
+import time
 import jinja2
 import numpy as np
 
@@ -38,14 +39,23 @@ class Page:
 
 
 def fake_web_pages(n_pages, mean_backlinks, std_backlinks):
+    print('Creating pages and backlink counts in memory...')
     pages = [Page(f"page_{i}_{uuid.uuid4()}.html") for i in range(n_pages)]
     ipages = np.arange(len(pages))
     backlinks_per_page = np.random.normal(mean_backlinks, std_backlinks, len(pages))
+    print('OK')
+    count = 0
+    g = np.random.Generator(np.random.PCG64())
     for page, n_backlinks in zip(pages, backlinks_per_page):
+        count += 1
+        if count % 10000 == 0:
+            print(f'[backlinks-loop] time={time.time():.2f}, count={count} ({count/n_pages*100.0:.2f}%)')
         n_backlinks = max(0, int(round(n_backlinks)))
         page._n_backlinks = n_backlinks
-        for backlink_ix in np.random.choice(ipages, n_backlinks, replace=False):
-            pages[backlink_ix].links.add(page.filename)
+        if n_backlinks > 0:
+            selected_backlinks = g.choice(ipages, n_backlinks, replace=False)
+            for backlink_ix in selected_backlinks:
+                pages[backlink_ix].links.add(page.filename)
     for i in ipages:
         yield i, pages[i]
 
@@ -66,7 +76,7 @@ def main():
 
     for i, page in fake_web_pages(n_pages, mean_backlinks, std_backlinks):
         output_path = os.path.join(output_dir, page.filename)
-        print(f"Rendering page {i}: {page.filename} to {output_path}")
+        print(f"[{(i+1)/n_pages*100:.2f}% - {time.time():.2f}] Rendering to {output_path}: page={page.filename}, backlinks={page._n_backlinks}, links={len(page.links)}")
         index_links.add(page.filename)
         with open(output_path, "w", encoding="utf-8") as f:
             f.write(page.render_html())
